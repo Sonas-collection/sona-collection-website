66d9d6f1d8494d74a626fe745f69f1e3e23e8405
Sonas-collection <sonaliratnakar3@gmail.com>
2026-07-28 02:13:23 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,11 +1,21 @@
 const products = [
+
 {
 id:1,
-name:"Floral Printed Short Kurti",
+
+name:"Floral Bloom Short Kurti",
+
 price:"Contact for Price",
+
 image:"kurti1.jpeg",
+
 sizes:["S","M","L","XL","XXL"],
+
 category:"Short Kurti",
-badge:"NEW"
+
+badge:"NEW",
+
+description:"Elegant floral printed short kurti perfect for daily wear."
 }
+
 ];
