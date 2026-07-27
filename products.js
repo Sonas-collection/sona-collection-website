d454f21fd53509fca58fd64e704151f7e041879e
Sonas-collection <sonaliratnakar3@gmail.com>
2026-07-27 21:17:20 +0530

    Added products.js

---
+++ products.js
@@ -0,0 +1,11 @@
+const products = [
+{
+id:1,
+name:"Floral Printed Short Kurti",
+price:"Contact for Price",
+image:"assets/products/kurti1.jpg",
+sizes:["S","M","L","XL","XXL"],
+category:"Short Kurti",
+badge:"NEW"
+}
+];
